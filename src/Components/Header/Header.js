cd38c6cc752b352ab94f4d997e2fae647817cffb
ketherball <121590283+ketherball@users.noreply.github.com>
2023-10-03 16:40:22 -0400

    feat: move header img to component background

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,12 +4,9 @@ import reservationImg from "../../images/reservations.jpg";
 function Header() {
     return(
         <header>
-            <section>
-                <h1>Reservations</h1>
-                <h2>Book Ahead!</h2>
-                <p>Just fill out the form below and we will find you available seating.</p>
-            </section>
-            <img src={reservationImg} alt="Reserved table for you!"/>
+            <h1>Reservations</h1>
+            <h2>Book Ahead!</h2>
+            <p>Just fill out the form below and we will find you available seating.</p>
         </header>
     );
 }
