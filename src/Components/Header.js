bebd45806f0dc360ca6a753ad21bf45156a8c080
ketherball <121590283+ketherball@users.noreply.github.com>
2023-10-03 15:32:13 -0400

    feat: add google fonts

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,6 +3,8 @@ function Header() {
     return(
         <header>
             <h1>Reservations</h1>
+            <h2>Book Ahead!</h2>
+            <p>Just fill out the form below and we will find you available seating.</p>
         </header>
     );
 }
