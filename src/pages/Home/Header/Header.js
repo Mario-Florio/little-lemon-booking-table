dc5cfe6474314274193d6deab354a974d140a598
ketherball <121590283+ketherball@users.noreply.github.com>
2023-10-03 19:46:07 -0400

    feat: add img to header
    Extract general header styling from reservations stylesheet into app stylesheet.

--- src/pages/Home/Header/Header.js
+++ src/pages/Home/Header/Header.js
@@ -1,12 +1,16 @@
 import "./Header.css";
+import headerImg from "./header.jpg";
 
 function Header() {
     return(
         <header className="home__header">
-            <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-            <button>Reserve a Table</button>
+            <section>
+                <h1>Little Lemon</h1>
+                <h2>Chicago</h2>
+                <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                <a href="/reservations">Reserve a Table</a>
+            </section>
+            <img src={headerImg} alt="Waiter holding dish of food."/>
         </header>
     );
 }
