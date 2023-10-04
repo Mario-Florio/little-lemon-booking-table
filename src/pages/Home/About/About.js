2f2bd5e30a010fa84d4e3914db060f8b5e107f64
ketherball <121590283+ketherball@users.noreply.github.com>
2023-10-04 13:36:48 -0400

    feafeeat: style about section

--- src/pages/Home/About/About.js
+++ src/pages/Home/About/About.js
@@ -1,4 +1,6 @@
 import "./About.css";
+import chefsImg from "./images/chefs.jpg";
+import restaurantImg from "./images/restaurant.jpg";
 
 function About() {
     return(
@@ -6,11 +8,16 @@ function About() {
             <article>
                 <h3>Little Lemon</h3>
                 <h4>Chicago</h4>
-                <p>Desc</p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat est velit egestas dui id ornare. Et netus et malesuada fames ac. Tellus integer feugiat scelerisque varius morbi enim nunc. Facilisi cras fermentum odio eu feugiat. Pharetra et ultrices neque ornare aenean euismod elementum. Integer feugiat scelerisque varius morbi enim nunc faucibus a.
+                </p>
+                <p>
+                    Iaculis urna id volutpat lacus laoreet non. Quis risus sed vulputate odio ut enim blandit volutpat. Leo urna molestie at elementum eu facilisis sed odio morbi. Est ante in nibh mauris cursus mattis molestie. In arcu cursus euismod quis viverra nibh. Vitae elementum curabitur vitae nunc. Netus et malesuada fames ac turpis egestas maecenas. Erat nam at lectus urna. Neque convallis a cras semper auctor neque vitae tempus.
+                </p>
             </article>
             <section>
-                <img src="" alt=""/>
-                <img src="" alt=""/>
+                <img className="restaurant" src={restaurantImg} alt=""/>
+                <img className="chefs" src={chefsImg} alt=""/>
             </section>
         </section>
     );
